23d10f702ea0d2e09aeeefaafb88638da609e176
Dominic <meshtatsuo@gmail.com>
2017-04-21 18:01:01 -0500

    fiddle that diddle
    :game_die:

--- js/siteAnim.js
+++ js/siteAnim.js
@@ -1,6 +1,24 @@
 $(document).ready(function(){
-  
-    $('#navBar').fadeIn(3000);
-    $('.pusher').fadeIn(3000);
-    
+   if ($(window).width() < 720) {
+    $('nav').removeClass('visible');
+    $('#menuButton').addClass('visible');   
+} else {
+}
 });
+
+
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
+
