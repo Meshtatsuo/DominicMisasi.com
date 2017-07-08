2bf7124dc370a08275d0a13f3d5cf091ebb04bb5
Dominic <meshtatsuo@gmail.com>
2017-07-08 10:58:09 -0500

    Live website push

---
+++ bin/contact_me.php
@@ -0,0 +1,21 @@
+<?php
+/*
+    $name=$_POST['fullName'];
+    $email=$_POST['emailAddress'];
+    $phone=$_POST['phoneNumber'];
+    $message=$_POST['message'];
+
+    $to="info@DominicMisasi.com"; //Email address to send email to
+    $subject="Form Submission!";
+    $message="Name:" .$name."\n"."Phone: ".$phone."\n". "Message: "."\n\n".$message;
+    $headers="From: ".$email;
+
+    if(mail($to, $subject, $message, $headers)){
+        echo success;
+    }
+    else {
+        echo error;
+    }
+*/
+   return error;
+?>
---
+++ contact.js
@@ -0,0 +1,59 @@
+$('.ui.form')
+  .form({
+    fields: {
+      name: {
+        identifier: 'fullName',
+        rules: [
+          {
+            type   : 'empty',
+            prompt : 'Please enter your name'
+          }
+        ]
+      },
+      emailAddress: {
+        identifier: 'emailAddress',
+        rules: [
+          {
+            type   : 'empty',
+            prompt : 'Please enter your email address'
+          }
+        ]
+      },
+      message: {
+        identifier: 'message',
+        rules: [
+          {
+            type   : 'empty',
+            prompt : 'Please enter a message'
+          },
+        ]
+      },
+    }
+  })
+$.ajax({
+                url: "./bin/contact_me.php",
+                type: "POST",
+                data: {
+                    name: fullName,
+                    phone: phoneNumber,
+                    email: emailAddress,
+                    message: message
+                },
+    cache: false,
+    success: function() {
+        // Success message
+       
+        
+        //clear form
+        ('#contactForm').form('clear');
+    },
+    error: function() {
+        // Failure Message
+        
+        //clear form
+        ('#contactForm').form('clear');
+        
+    },
+    
+    
+;
